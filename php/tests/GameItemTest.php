55ca6a9b652751328a8161220eb600cd6da2c039
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-10-27 18:25:51 +0100

    added gameitems entity test

---
+++ php/tests/GameItemTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use GildedRose\GameItem;
+use PHPUnit\Framework\TestCase;
+
+class GameItemTest extends TestCase
+{
+    public function canIncreaseQualityIncreaseProvider(): array
+    {
+        return [
+            [25, true],
+            [0, true],
+            [49, true],
+            [50, false],
+            [-1, false],
+        ];
+    }
+
+    /**
+     * @dataProvider canIncreaseQualityIncreaseProvider
+     */
+    public function testCanIncreaseQualityWhenValid(int $quality, bool $expected): void
+    {
+        $item = new GameItem('Aged Brie', 0, $quality);
+        $this->assertSame($expected, $item->canQualityBeIncreased());
+    }
+
+    public function testIsSellNegativeWhenNegative(): void
+    {
+        $item = new GameItem('Aged Brie', -1, 0);
+        $this->assertFalse($item->isSellNegative());
+    }
+
+    public function testIsSellNegativeWhenPositive(): void
+    {
+        $item = new GameItem('Aged Brie', 10, 0);
+        $this->assertFalse($item->isSellNegative());
+    }
+}

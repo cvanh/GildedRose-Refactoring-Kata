12c5aa83cda52c4bfce8fc2de8f9ef0bc3f8f40b
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-10-30 12:19:40 +0100

    remove dead code

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -43,48 +43,6 @@ final class GildedRose
                 default:
                     break;
             }
-
-            //     if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            //         if ($item->quality > 0) {
-            //             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-            //                 $item->quality = $item->quality - 1;
-            //             }
-            //         }
-            //     } else {
-            //         if ($item->quality < 50) {
-            //             $item->quality = $item->quality + 1;
-            //             if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-            //                 // increase the sellin by 2 when quality is between 6-10
-            //                 if ($item->sellIn < 11 || $item->sellIn < 6) {
-            //                     if ($item->quality < 50) {
-            //                         $item->quality = $item->quality + 1;
-            //                     }
-            //             }
-            //         }
-            //     }
-
-            //     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-            //         $item->sellIn = $item->sellIn - 1;
-            //     }
-
-            //     if ($item->sellIn < 0) {
-            //         if ($item->name != 'Aged Brie') {
-            //             if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            //                 if ($item->quality > 0) {
-            //                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-            //                         $item->quality = $item->quality - 1;
-            //                     }
-            //                 }
-            //             } else {
-            //                 $item->quality = $item->quality - $item->quality;
-            //             }
-            //         } else {
-            //             if ($item->quality < 50) {
-            //                 $item->quality = $item->quality + 1;
-            //             }
-            //         }
-            //     }
-            // }
         }
     }
 }
